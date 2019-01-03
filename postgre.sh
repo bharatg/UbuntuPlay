f738b875c4b6847093d696b6d9c50ef53c1fa162
bharatg <bharat.ggaikwad@gmail.com>
2019-01-03 18:52:07 +0530

    once in sudo mode donot write sudo word

--- postgre.sh
+++ postgre.sh
@@ -16,15 +16,10 @@ database_prefix=$project_name
 database_suffix="_prod"
 database_name=$database_prefix$database_suffix
 sudo su << EOFSU
-project_name="bharat"
-project_password="password"
-project_ip="127.0.0.1:8000"
-project_domain="104.196.68.173"
-
 su postgres<<EOF
 cd ~
-sudo createuser $project_name
-sudo createdb $database_name --owner $project_name
+createuser $project_name
+createdb $database_name --owner $project_name
 psql -c "ALTER USER $project_name WITH PASSWORD '$project_password'"
 EOF
 EOFSU
