2c683320df3c0a50bda6661c331b0192eae690a6
bharatg <bharat.ggaikwad@gmail.com>
2019-01-03 19:44:31 +0530

    Create gunicorn_play.sh

---
+++ gunicorn_play.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Ubuntu 16.04 LTS / Ubuntu 18.04 LTS
+# CONFIGURE THE FOLLOWING SECTION 
+# --------------------------------------------
+project_name="bharat"
+project_password="password"
+project_ip="127.0.0.1:8000"
+project_domain="104.196.68.173"
+
+
+# Create gunicorn_start file
+echo "[DJANGOGO] CONFIGURING GUNICORN..."
+cd /home/$project_name/bin
+sudo cat << EOF >> gunicorn_start
+#!/bin/bash
+NAME="$project_name"
+DIR=/home/$project_name/$project_name
+USER=$project_name
+GROUP=$project_name
+WORKERS=3
+BIND=unix:/home/$project_name/run/gunicorn.sock
+DJANGO_SETTINGS_MODULE=project.settings
+DJANGO_WSGI_MODULE=project.wsgi
+LOG_LEVEL=error
+cd \$DIR
+source /home/$project_name/bin/activate
+export DJANGO_SETTINGS_MODULE=\$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=\$DIR:\$PYTHONPATH
+exec /home/$project_name/bin/gunicorn \${DJANGO_WSGI_MODULE}:application \\
+  --name \$NAME \\
+  --workers \$WORKERS \\
+  --user=\$USER \\
+  --group=\$GROUP \\
+  --bind=\$BIND \\
+  --log-level=\$LOG_LEVEL \\
+  --log-file=-
+EOF
