54a5ba634e054483a2a1ae23b927f34bb57dda03
bharatg <bharat.ggaikwad@gmail.com>
2019-01-03 11:15:10 +0530

    Create postgre.sh

---
+++ postgre.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Ubuntu 16.04 LTS / Ubuntu 18.04 LTS
+# CONFIGURE THE FOLLOWING SECTION 
+# --------------------------------------------
+project_name="bharat"
+project_password="password"
+project_ip="127.0.0.1:8000"
+project_domain="104.196.68.173"
+
+
+# Create Postgres
+echo "[DJANGOGO] INSTALL & CONFIGURE POSTGRES..."
+sudo apt-get -y install postgresql postgresql-contrib
+database_prefix=$project_name
+database_suffix="_prod"
+database_name=$database_prefix$database_suffix
+sudo su << EOFSU
+su postgres<<EOF
+cd ~
+sudo createuser $project_name
+sudo createdb $database_name --owner $project_name
+psql -c "ALTER USER $project_name WITH PASSWORD '$project_password'"
+EOF
+EOFSU
