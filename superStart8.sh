35243672f53238c73b4a84db343147a2dc010b27
bharatg <bharat.ggaikwad@gmail.com>
2019-01-03 20:04:06 +0530

    Create superStart8.sh

---
+++ superStart8.sh
@@ -0,0 +1,6 @@
+# Restart Supervisor
+echo "[DJANGOGO] RESTARTING SUPERVISOR..."
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl status $project_name
+sudo supervisorctl restart $project_name
