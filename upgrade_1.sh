6012338f08d756bf6360bedd98f78be8e1f3738c
bharatg <bharat.ggaikwad@gmail.com>
2019-01-03 10:52:13 +0530

    Update upgrade_1.sh

--- upgrade_1.sh
+++ upgrade_1.sh
@@ -3,7 +3,7 @@
 # Ubuntu 16.04 LTS / Ubuntu 18.04 LTS
 # CONFIGURE THE FOLLOWING SECTION 
 # --------------------------------------------
-project_name="bharat"xx
+project_name="bharat"
 project_password="password"
 project_ip="127.0.0.1:8000"
 project_domain="104.196.53.61"
