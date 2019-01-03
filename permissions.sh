24b9e86b8ba58fdc4c634adb4d28cf9ea59bf91d
bharatg <bharat.ggaikwad@gmail.com>
2019-01-03 19:56:39 +0530

    Update permissions.sh

--- permissions.sh
+++ permissions.sh
@@ -1,3 +1,13 @@
+#!/bin/sh
+
+# Ubuntu 16.04 LTS / Ubuntu 18.04 LTS
+# CONFIGURE THE FOLLOWING SECTION 
+# --------------------------------------------
+project_name="bharat"
+project_password="password"
+project_ip="127.0.0.1:8000"
+project_domain="104.196.68.173"
+
 sudo su << EOFSU
 cd /home/$project_name/bin
 # Set permissions on gunicorn_start file and create gunicorn logs
